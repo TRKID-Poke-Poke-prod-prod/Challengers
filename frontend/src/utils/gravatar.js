9c39b10884aef3aafe4e1bf8957f8fa756819521
TRKID-Poke-Poke-prod-prod <102126010+TRKID-Poke-Poke-prod-prod@users.noreply.github.com>
2025-10-26 10:32:58 +1100

    Delete frontend directory

--- frontend/src/utils/gravatar.js
+++
@@ -1,6 +0,0 @@
-import md5 from "md5";
-
-export const getGravatar = (email) => {
-    const hash = md5(email.trim().toLowerCase());
-    return `https://www.gravatar.com/avatar/${hash}?d=identicon`;
-};
